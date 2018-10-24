9e4f2c7a934e3953a4cc3ab7dee76405179c1e4b
Manoj0048 <43470956+Manoj0048@users.noreply.github.com>
2018-10-24 15:44:06 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

---
+++ module7/7.1 Steque/Solution.java
@@ -0,0 +1,116 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class StackQueue {
+
+	int top;
+	int rear;
+	int front;
+	LinkedList<Integer> ll = new LinkedList<>();
+
+	public StackQueue() {
+		top = -1;
+		rear = 0;
+		front = -1;
+	}
+
+	boolean push(int x) {
+
+		ll.add(top + 1, x);
+		// System.out.println(ll);
+		String s = ll.toString();
+		System.out.println(s.substring(1, s.length() - 1));
+
+		return false;
+
+	}
+
+	boolean enqueue(int x) {
+
+		rear = ll.size();
+		ll.add(rear++, x);
+		// System.out.println(ll);
+		String s = ll.toString();
+		System.out.println(s.substring(1, s.length() - 1));
+
+		return false;
+
+	}
+
+	int pop() {
+		Scanner sc = new Scanner(System.in);
+
+		if (ll.isEmpty()) {
+			System.out.println("Steque is empty.");
+
+		} else {
+			// System.out.println(ll.size());
+			ll.remove(front + 1);
+			// System.out.println(ll);
+			String s = ll.toString();
+			if (!(ll.isEmpty())) {
+				System.out.println(s.substring(1, s.length() - 1));
+			} else {
+				System.out.println("Steque is empty.");
+			}
+
+		}
+		return 1;
+
+	}
+}
+
+public class Solution {
+
+	public static void main(String[] args) {
+		StackQueue sq = new StackQueue();
+		Scanner sc = new Scanner(System.in);
+		int operations = sc.nextInt();
+		// sc.next();
+
+		// System.out.println("input - "+i);
+		// sc.next();
+
+		for (int i = 0; i < operations + 1; i++) {
+			// System.out.println();
+			
+
+			do {
+
+				String in = sc.nextLine().trim();
+//				System.out.println(in.length());
+
+				if (in.length() == 0) {
+					break;
+				}
+
+				String inp[] = in.split(" ");
+				switch (inp[0]) {
+				case "push":
+//					int y=sc.nextInt();
+//					System.out.println(Integer.parseInt(inp[1]));
+					sq.push(Integer.parseInt(inp[1]));
+					break;
+
+				case "enqueue":
+					sq.enqueue(Integer.parseInt(inp[1]));
+					break;
+
+				case "pop":
+
+					sq.pop();
+					// sc.nextLine();
+					break;
+
+				}
+
+			} while (sc.hasNextLine());
+			if(i>0)
+			System.out.println("");
+			sq.ll.clear();
+
+		}
+
+	}
+
+}
